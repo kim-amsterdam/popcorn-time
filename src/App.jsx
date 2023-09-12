6daf58e719bc6de26593bfbbab3473dc64c47a68
Kim Idsinga <kimidsinga@gmail.com>
2023-09-12 11:53:17 +0200

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from './components/header'
+import Main from './components/main'
+import Footer from './components/footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <> 
+<Header />
+<Main />
+<Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/main.jsx
@@ -0,0 +1,7 @@
+function Main() {
+    return (
+        <h1>This is the main content</h1>
+    )
+}
+
+export default Main;
